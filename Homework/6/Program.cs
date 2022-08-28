6444dbf7cf5dea3b829f3e1646ffd6e67e0ad818
Anastasiya Avddeva <freesoul89@mail.ru>
2022-08-28 16:07:47 +0300

    6 zadacha

---
+++ Homework/6/Program.cs
@@ -0,0 +1,30 @@
+﻿int numberA = 2;
+float  temp;
+temp = numberA%2;
+
+if (temp!=0)
+   Console.WriteLine("A не делится на цело");
+else 
+
+   Console.WriteLine("A делится на цело");
+
+int numberB = -3;
+float  temp1;
+temp1 = numberB%2;
+
+if (temp1!=0)
+
+   Console.WriteLine("B не делится на цело");
+else 
+
+   Console.WriteLine("B делится на цело");
+
+   int numberC = 7;
+float  temp2;
+temp2 = numberC%2;
+
+if (temp2!=0)
+   Console.WriteLine("C не делится на цело");
+else 
+
+   Console.WriteLine("C делится на цело");
