58305a0839c30012194a5855e658351f7bf0b986
Anastasiya Avddeva <freesoul89@mail.ru>
2022-08-28 16:08:49 +0300

    8 zadacha

---
+++ Homework/8/Program.cs
@@ -0,0 +1,13 @@
+﻿double N=0;
+int i=0;
+string s;
+Console.Write("Введите N= ");
+s = Console.ReadLine();
+N=Int32.Parse(s);
+//Console.WriteLine (N); 
+
+for (i=0; i<=N; i++) {
+    if (i%2==0)
+        Console.WriteLine (i); 
+   
+}
