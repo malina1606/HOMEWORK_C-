a235842d6c00a9b9c026292e65d4512e6dd2a0e5
Anastasiya Avddeva <freesoul89@mail.ru>
2022-08-28 18:49:28 +0300

    1 commit

---
+++ Homework/2seminar/1/Program.cs
@@ -0,0 +1,11 @@
+﻿int start = 100;
+int stop = 1000;
+int value = new Random().Next(start,stop);
+Console.Write("Случайное число ", value);
+Console.WriteLine(value);
+int digit_first = value / 100;
+int digit_second = (value % 100) / 10;
+int digit_third = value % 10;
+
+Console.Write("Вторая цифра ", digit_second);
+Console.WriteLine(digit_second);
