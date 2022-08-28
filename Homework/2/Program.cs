aa9635719240ddc2664884273737bda5d22d31c5
Anastasiya Avddeva <freesoul89@mail.ru>
2022-08-28 16:01:05 +0300

    2 zadacha

---
+++ Homework/2/Program.cs
@@ -0,0 +1,49 @@
+﻿int numberA = 5;
+int numberB = 7;
+int max =0;
+
+if (numberA > numberB)
+    max = numberA;
+    else
+    max = numberB;
+
+Console.Write("A= ");
+Console.Write(numberA);
+Console.Write("  ");
+Console.Write("B= ");
+Console.Write(numberB);
+Console.Write("  ");
+Console.Write("max = ");
+Console.WriteLine(max);
+
+numberA = 2;
+numberB = 10;
+max =0;
+if (numberA > numberB)
+    max = numberA;
+    else
+    max = numberB;
+Console.Write("A= ");
+Console.Write(numberA);
+Console.Write("  ");
+Console.Write("B= ");
+Console.Write(numberB);
+Console.Write("  ");
+Console.Write("max = ");
+Console.WriteLine(max);
+
+numberA = -9;
+numberB = -3;
+max =0;
+if (numberA > numberB)
+    max = numberA;
+    else
+    max = numberB;
+Console.Write("A= ");
+Console.Write(numberA);
+Console.Write("  ");
+Console.Write("B= ");
+Console.Write(numberB);
+Console.Write("  ");
+Console.Write("max = ");
+Console.Write(max);
