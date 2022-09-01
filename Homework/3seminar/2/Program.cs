20e6f239f43d1b58a035d98758444b4c90a4f51f
Anastasiya Avddeva <freesoul89@mail.ru>
2022-09-01 05:18:51 +0300

    Tasks 3 seminar

---
+++ Homework/3seminar/2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.*/
+
+int x,y,z;
+double D;
+int[] a = new int [3]; 
+Console.WriteLine("Введите координаты точки А через пробел: ");
+string strA = Console.ReadLine();
+string [] coordA = strA.Split(' ');
+int[] intsA = Array.ConvertAll(coordA, strA => int.Parse(strA));// преобразование массива одного типа(string) в массив другого типа (int)
+
+Console.WriteLine("Введите координаты точки B через пробел: ");
+string strB = Console.ReadLine();
+string [] coordB = strB.Split(' ');
+int[] intsB = Array.ConvertAll(coordB, strB => int.Parse(strB));
+
+
+x = (intsB[0]-intsA[0])*(intsB[0]-intsA[0]);
+y = (intsB[1]-intsA[1])*(intsB[1]-intsA[1]);
+z = (intsB[2]-intsA[2])*(intsB[2]-intsA[2]);
+D = Math.Sqrt(x+y+z);
+Console.Write("D= ");
+Console.WriteLine(D);
---
+++ Homework/3seminar/3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.*/
+int i=0;
+int num =1;
+Console.WriteLine("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+while (i < N){
+    array[i]=num * num * num;
+    Console.Write(array[i]+" ");
+    num++;
+    i++;
+}
