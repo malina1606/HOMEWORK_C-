23065db3ee8af5ce9225e75e59cfec601de5bb28
Anastasiya Avddeva <freesoul89@mail.ru>
2022-09-01 20:35:16 +0300

    4 seminar

---
+++ Homework/4seminar/1/Program.cs
@@ -0,0 +1,12 @@
+﻿ /*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow()*/
+
+int result =1;
+Console.WriteLine("Введите натуральное число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i <= B; i++){ 
+    result *= A;
+
+}
+Console.WriteLine("A^B =" + result);
---
+++ Homework/4seminar/2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе, которые являются делителями этого числа. 
+(для получения цифр числа операцию приведения числа к строке не использовать)*/
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int countDigits = 0;
+int tmp1=A;
+int tmp2=A;
+int sum =0;
+int index = 0;
+
+while (tmp1 != 0) {
+    countDigits++;
+    tmp1 = tmp1 /10;
+}
+int [] array = new int [countDigits];
+while (tmp2 != 0) {
+    array[index] = tmp2 % 10;
+    tmp2 /= 10;
+    index++;
+}
+
+for(int i=0; i < countDigits; i++)
+    if (A % array[i]==0) {
+        sum+=array[i];
+    }
+Console.WriteLine("Сумма делителей равна " + sum);
